1efa16ec49e19ab10c3c5ead1b6fa1cead4966cd
ropelletier <88781432+ropelletier@users.noreply.github.com>
2024-02-02 11:24:25 -0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -4,9 +4,11 @@ if [ "$CURSOR" = "true" ];
 then
     echo  "Starting X with cursor enabled"
     #exec startx -- $FORCE_DISPLAY &
-    startx cctv-viewer
+    startx &
+    cctv-viewer
 else
     echo  "Starting X with cursor disabled"
     #exec startx -- $FORCE_DISPLAY -nocursor
-    startx cctv-viewer
+    startx &
+    cctv-viewer
 fi
