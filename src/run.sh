09b2f03409254f288b84e710dd471ef110423770
ropelletier <88781432+ropelletier@users.noreply.github.com>
2024-02-02 11:43:11 -0500

    Update run.sh

--- src/run.sh
+++ src/run.sh
@@ -4,9 +4,7 @@ echo "Starting xserver"
 if [ "$CURSOR" = "true" ];
 then
     echo  "Starting X with cursor enabled"
-    #exec startx -- $FORCE_DISPLAY &
-    startx -- cctv-viewer 
-    
+    exec startx -- $FORCE_DISPLAY    -nocursor
 else
     echo  "Starting X with cursor disabled"
     #exec startx -- $FORCE_DISPLAY -nocursor
